918a3509a3fd314f0dc90876545a55ebf753022a
Yanying Wang <yanyingwang@users.noreply.github.com>
2022-10-09 12:48:29 +0800

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -5,4 +5,5 @@
 (define scribblings '(("scribblings/colorize.scrbl" ())))
 (define pkg-desc "colorize your console")
 (define version "0.1")
-(define pkg-authors '(yanyingwang))
+(define pkg-authors '(yanyingwang)
+(define license '(Apache-2.0))
